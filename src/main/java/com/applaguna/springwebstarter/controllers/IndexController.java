d9814983b791db9f2779ed628dd7e2b4244dea60
Ricardo Roman <ricardomon85@gmail.com>
2021-04-04 13:02:22 -0500

    agregar datos a l vista

--- src/main/java/com/applaguna/springwebstarter/controllers/IndexController.java
+++ src/main/java/com/applaguna/springwebstarter/controllers/IndexController.java
@@ -1,13 +1,18 @@
 package com.applaguna.springwebstarter.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-    @GetMapping({"/", "/index"})
-    public String index() {
+    @GetMapping({"/", "/index", ""})
+    public String index(Model model) {
+        model.addAttribute("titulo", "Hola Spring framework relativo");
+
         return "index";
     }
 }
